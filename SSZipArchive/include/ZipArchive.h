dd46725dd826a3e10a00db97b38185f83e404b2a
Neil <shengzhi.chen@ubnt.com>
2020-11-24 15:47:58 +0800

    fix: merge comflict

--- SSZipArchive/include/ZipArchive.h
+++ SSZipArchive/include/ZipArchive.h
@@ -16,8 +16,8 @@ FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SSZipArchive.h>
 
 // This is to account for the many different ways this library gets imported.
-#if __has_include(<SSZipArchive/SSZipArchive.h>)
-#import <SSZipArchive/SSZipArchive.h>
+#if __has_include(<FRSSZipArchive/SSZipArchive.h>)
+#import <FRSSZipArchive/SSZipArchive.h>
 #elif __has_include("../SSZipArchive.h")
 #import "../SSZipArchive.h"
 #else
